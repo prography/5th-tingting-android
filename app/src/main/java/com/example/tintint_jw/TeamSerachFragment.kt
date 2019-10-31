a31f1ca66b7f9b623efdeb1b485e3430320fd630
chlwjddn9181@naver.com <wjdgns822!>
2019-10-31 16:23:19 +0900

    initial tingting

---
+++ app/src/main/java/com/example/tintint_jw/TeamSerachFragment.kt
@@ -0,0 +1,17 @@
+package com.example.tintint_jw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class TeamSerachFragment :Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val view = inflater.inflate(R.layout.activity_team_serach_fragment,null)
+        return view
+    }
+}
