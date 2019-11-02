8c9bdd34bc87442762fb530de820ab5524c83171
WOO <chlwjddn9181@naver.com>
2019-11-02 16:20:35 +0900

    add splash and login and register picture without function.

--- app/src/main/java/com/example/tintint_jw/View/LoginActivity.kt
+++ app/src/main/java/com/example/tintint_jw/View/LoginActivity.kt
@@ -2,22 +2,41 @@ package com.example.tintint_jw.View
 
 import android.app.Application
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tintint_jw.R
+import com.example.tintint_jw.SharedPreference.SharedPreference
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    companion object {
+        lateinit var prefs : SharedPreference
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        //여기 자동로그인이랑 비교 구문 구현.
+        prefs = SharedPreference(applicationContext)
+
+        if((prefs.myId=="서버로 부터 불러온 아이디") && (prefs.myPw=="서버로 부터 불러온 PW")){
+            val intent = Intent(applicationContext,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        signIn.setOnClickListener(){
+        //    if(loginId.text.equals("서버로 부터 불러 온 id") && loginPw.text.equals("서버로 부터 불러 온 pw")){
+                prefs.myId = loginId.text.toString()
+                prefs.myPw = loginPw.text.toString()
+       //     }
+        }
 
         signUp.setOnClickListener(){
             val intent = Intent(applicationContext,SignUpActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
---
+++ app/src/main/java/com/example/tintint_jw/View/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tintint_jw.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tintint_jw.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
