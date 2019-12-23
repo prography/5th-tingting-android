1d130058b1332ed4d60cce246b603ee02d291557
Hyemin Lyeo <irenelyeo@gmail.com>
2019-12-23 10:09:21 +0900

    revise ui and searchTeam design & all btn bug fix

--- app/src/main/java/com/example/tintint_jw/SearchTeam/SearchTeamFragment.kt
+++ app/src/main/java/com/example/tintint_jw/SearchTeam/SearchTeamFragment.kt
@@ -27,33 +27,33 @@ class SearchTeamFragment : Fragment() {
         var Adapter = SearchTeamAdapter(activity!!.applicationContext, searchList)
         // 서버로 부터 데이터 셋이 왔을 때
         // 데이터 몇개를 불러올지, 갱신을 어떻게 할지 생각 필요.
-        searchListDataset.add(SearchTeamData(R.drawable.iu3, "1:1/재밌게 놀아요 ~", 1))
         searchListDataset.add(
             SearchTeamData(
-                R.drawable.suzy1,
-                R.drawable.naeun1,
-                R.drawable.iu2,
-                R.drawable.seulgi,
+                R.drawable.seungho,
+                R.drawable.jongsuk1,
+                R.drawable.woobin1,
+                R.drawable.gray_fill,
                 "4:4/7시 홍대 환영",
                 4
             )
         )
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.iu, R.drawable.seulgi, "2:2/건입 2명 !!", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.woobin1, R.drawable.jongsuk1, R.drawable.gray_fill, "3:3/달리즈아", 3))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, "2:2/건입 2명 !!", 2))
         searchListDataset.add(
             SearchTeamData(
-                R.drawable.suzy2,
-                R.drawable.naeun1,
-                R.drawable.iu,
+                R.drawable.jongsuk1,
+                R.drawable.gyunsang,
+                R.drawable.gray_fill,
                 "3:3/강남 ㄱㄱ",
                 3
             )
         )
-        searchListDataset.add(SearchTeamData(R.drawable.iu, R.drawable.seulgi, "2:2/건입 2명 !!", 2))
-        searchListDataset.add(SearchTeamData(R.drawable.iu3, "1:1/재밌게 놀아요 ~", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
+        searchListDataset.add(SearchTeamData(R.drawable.woobin1, R.drawable.jongsuk1, R.drawable.gray_fill, R.drawable.gray_fill,"4:4/건입거닙!!", 4))
+        searchListDataset.add(SearchTeamData(R.drawable.gyunsang, R.drawable.gray_fill, "2:2/재밌게 놀아요 ~", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, R.drawable.gray_fill,"3:3/강남이면 컴컴", 3))
+        searchListDataset.add(SearchTeamData(R.drawable.jongsuk1, R.drawable.gray_fill,"2:2/홍대 근처 사시는 분", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.gyunsang, R.drawable.gray_fill, "2:2/소주 잘먹", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, R.drawable.gray_fill,"3:3/심심해여", 3))
 
         view.segmentation_button.setTintColor(
             resources.getColor(R.color.tingtingMain),
@@ -74,6 +74,11 @@ class SearchTeamFragment : Fragment() {
 
         view.memberAll.setOnClickListener {
             searchList.clear()
+
+            for(i in 0..searchListDataset.size-1){
+                searchList.add(searchListDataset.get(i))
+            }
+
             view.searchTeamRecyclerView.adapter = Adapter
         }
 
