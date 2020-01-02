0333541d122481c43b9d75619aa18214e45b8019
WOO <chlwjddn9181@naver.com>
2020-01-02 19:32:30 +0900

    merge
    spinner
    change profile activity

--- app/src/main/java/com/example/tintint_jw/Matching/MatchingFragment.kt
+++ app/src/main/java/com/example/tintint_jw/Matching/MatchingFragment.kt
@@ -1,12 +1,16 @@
 package com.example.tintint_jw.Matching
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tintint_jw.R
+import kotlinx.android.synthetic.main.fragment_matching_main.*
 import kotlinx.android.synthetic.main.fragment_matching_main.view.*
 
 
@@ -25,7 +29,7 @@ class MatchingFragment : Fragment() {
         }*/
 
         // 임시 데이터
-        teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
+        /*teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
         teamList.add(TeamData(R.drawable.haein, "연기대학교 1명/12월 1일/신촌","신촌에서 잠깐 놀 사람?"))
         teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
         teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
@@ -34,14 +38,60 @@ class MatchingFragment : Fragment() {
         teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
         teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
         teamList.add(TeamData(R.drawable.suzy2, R.drawable.suzy1, R.drawable.seulgi, R.drawable.iu,"한국대학교 4명/11월 29일/강남","안녕하세요! 같이 맥주나 한잔 고고"))
+*/
 
+        teamList.add(
+            TeamData(
+                R.drawable.iu3,
+                R.drawable.suzy2,
+                "놀사람",
+                "#강남 #8시 #칵테일 #좋아")
+        )
+
+        teamList.add(
+            TeamData(
+                R.drawable.naeun1,
+                R.drawable.iu,
+                R.drawable.seulgi,
+                "3명컴",
+                "#3:3 #홍대 #같이 #놀자 #심심해")
+        )
+
+        teamList.add(
+            TeamData(R.drawable.suzy1,
+                R.drawable.seulgi,
+                R.drawable.iu,
+                R.drawable.naeun1,
+                "마셔마셔",
+                "#홍대 #환영 #초저녁")
+        )
+
+        teamList.add(
+            TeamData(R.drawable.naeun1,
+                R.drawable.iu2,
+                R.drawable.seulgi,
+                "신촌고고",
+                "#신촌 #7시 #소맥")
+        )
+
+        teamList.add(
+            TeamData(R.drawable.seulgi,
+                R.drawable.iu2,
+                R.drawable.naeun1,
+                R.drawable.suzy2,
+                "건입거닙",
+                "#건입 #아무때나")
+        )
         val adapter = MatchingAdapter(activity!!.applicationContext, teamList)
 
         adapter.notifyDataSetChanged()
 
         adapter.matchingclick = object  : MatchingAdapter.MatchingClick{
             override fun Onclick(view: View, position: Int) {
-                activity!!.supportFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.mainFragment,MatchingDetail()).commit()
+
+                val intent = Intent(activity, MatchingDetail::class.java)
+                activity!!.startActivity(intent)
+                //activity!!.supportFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.mainFragment,MatchingDetail()).commit()
             }
         }
 
@@ -51,6 +101,26 @@ class MatchingFragment : Fragment() {
         val layoutManager = LinearLayoutManager(this.context)
         view.searchMatching.layoutManager = layoutManager
         view.searchMatching.setHasFixedSize(true)
+
+        // 팀 스피너
+        var spinner = view.filter
+
+        val listOptions = arrayOf("불금불금", "귀요미드드들", "마셔마셔")
+        val spinnerAdapter:FilterAdapter = FilterAdapter(context!!, listOptions)
+        spinner?.adapter = spinnerAdapter
+
+        // 스피너 아이템 이벤트
+        spinner?.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                currentTeam.setText(parent!!.getItemAtPosition(position).toString())
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+
+            }
+
+        })
 
         return view
     }
--- app/src/main/java/com/example/tintint_jw/SearchTeam/SearchTeamFragment.kt
+++ app/src/main/java/com/example/tintint_jw/SearchTeam/SearchTeamFragment.kt
@@ -6,8 +6,10 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.tintint_jw.MakeTeam.MakeTeam
 import com.example.tintint_jw.R
 import com.example.tintint_jw.TeamInfo.TeamInfoDetailActivity
@@ -17,6 +19,7 @@ class SearchTeamFragment : Fragment() {
 
     var searchListDataset = arrayListOf<SearchTeamData>()
     var searchList = arrayListOf<SearchTeamData>()
+    var isLoading :Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,43 +27,46 @@ class SearchTeamFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_search_team, null)
         view.memberAll.isSelected = true
-        var Adapter = SearchTeamAdapter(activity!!.applicationContext, searchList)
+
+        Adapter = SearchTeamAdapter(activity!!.applicationContext, searchList)
+
         // 서버로 부터 데이터 셋이 왔을 때
         // 데이터 몇개를 불러올지, 갱신을 어떻게 할지 생각 필요.
-        searchListDataset.add(SearchTeamData(R.drawable.iu3, "1:1/재밌게 놀아요 ~", 1))
         searchListDataset.add(
             SearchTeamData(
-                R.drawable.suzy1,
-                R.drawable.naeun1,
-                R.drawable.iu2,
-                R.drawable.seulgi,
-                "4:4/7시 홍대 환영",
+                R.drawable.seungho,
+                R.drawable.jongsuk1,
+                R.drawable.woobin1,
+                R.drawable.gray_fill,
+                "7시 홍대 환영",
                 4
             )
         )
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.iu, R.drawable.seulgi, "2:2/건입 2명 !!", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.woobin1, R.drawable.jongsuk1, R.drawable.gray_fill, "달리즈아", 3))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, "건입 2명 !!", 2))
         searchListDataset.add(
             SearchTeamData(
-                R.drawable.suzy2,
-                R.drawable.naeun1,
-                R.drawable.iu,
-                "3:3/강남 ㄱㄱ",
+                R.drawable.jongsuk1,
+                R.drawable.gyunsang,
+                R.drawable.gray_fill,
+                "강남 ㄱㄱ",
                 3
             )
         )
-        searchListDataset.add(SearchTeamData(R.drawable.iu, R.drawable.seulgi, "2:2/건입 2명 !!", 2))
-        searchListDataset.add(SearchTeamData(R.drawable.iu3, "1:1/재밌게 놀아요 ~", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
-        searchListDataset.add(SearchTeamData(R.drawable.seulgi, "1:1/맥주 한잔 해요 ^.^", 1))
+        searchListDataset.add(SearchTeamData(R.drawable.woobin1, R.drawable.jongsuk1, R.drawable.gray_fill, R.drawable.gray_fill,"건입거닙!!", 4))
+        searchListDataset.add(SearchTeamData(R.drawable.gyunsang, R.drawable.gray_fill, "재밌게 놀아요 ~", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, R.drawable.gray_fill,"강남이면 컴컴", 3))
+        searchListDataset.add(SearchTeamData(R.drawable.jongsuk1, R.drawable.gray_fill,"홍대 근처 사시는 분", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.gyunsang, R.drawable.gray_fill, "소주 잘먹", 2))
+        searchListDataset.add(SearchTeamData(R.drawable.seungho, R.drawable.gray_fill, R.drawable.gray_fill,"심심해여", 3))
 
+        //1명 2명 3명 선택하는 버튼
         view.segmentation_button.setTintColor(
             resources.getColor(R.color.tingtingMain),
             resources.getColor(R.color.white)
         )
 
-        //정보를 받아야 됨
+        //정보를 받아야 됨, 팀 만들기 버튼
         view.createTeamBtn.setOnClickListener {
 
             var intent = Intent(activity,MakeTeam::class.java)
@@ -74,7 +80,13 @@ class SearchTeamFragment : Fragment() {
 
         view.memberAll.setOnClickListener {
             searchList.clear()
+
+            for(i in 0..searchListDataset.size-1){
+                searchList.add(searchListDataset.get(i))
+            }
+
             view.searchTeamRecyclerView.adapter = Adapter
+
         }
 
         view.member2.setOnClickListener {
@@ -120,7 +132,45 @@ class SearchTeamFragment : Fragment() {
             }
         }
         return view
+
+
+        // loading 함수 기능.
+
+      view.searchTeamRecyclerView?.addOnScrollListener(object : PaginationScrollListener(LinearLayoutManager(view.context)){
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+
+                val visibleItemCount = view.searchTeamRecyclerView.layoutManager!!.childCount
+
+                val totalItemCount = layoutManager.itemCount
+
+                Log.d("list",visibleItemCount.toString())
+
+                super.onScrolled(recyclerView, dx, dy)
+            }
+
+            override fun isLastPage(): Boolean {
+
+                return isLastPage()
+            }
+
+            override fun isLoading(): Boolean {
+                return isLoading()
+            }
+
+            override fun loadMoreItems() {
+                isLoading = true
+                getMoreItem()
+            }
+
+        })
+
     }
+
+fun getMoreItem(){
+    isLoading = false
+}
+
 
 }
 
