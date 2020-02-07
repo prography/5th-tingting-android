cf8eb68d71290e1656ea81e8c7bad00648a55714
WOO <chlwjddn9181@naver.com>
2020-02-08 01:42:52 +0900

    add spiiner

--- app/src/main/java/com/tingting/ver01/Matching/MatchingFragment.kt
+++ app/src/main/java/com/tingting/ver01/Matching/MatchingFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.tingting.ver01.Model.Matching.ShowAllCandidateListResponse
@@ -24,9 +23,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import android.widget.AdapterView.OnItemSelectedListener
-
-
 
 
 class MatchingFragment : Fragment(), AdapterView.OnItemSelectedListener {
